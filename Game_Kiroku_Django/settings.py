31551196736919ee3d3d2b407d2698d3f0fa0fc5
Danila Olyanin <danilaolyanin@yandex.ru>
2024-12-02 06:19:37 +0300

    Fix Django debug toolbar

--- Game_Kiroku_Django/settings.py
+++ Game_Kiroku_Django/settings.py
@@ -89,7 +89,6 @@ DATABASES = {
         'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
         'HOST': os.environ.get('POSTGRES_HOST'),
         'PORT': os.environ.get('POSTGRES_PORT'),
-
     }
 }
 
@@ -134,3 +133,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+INTERNAL_IPS = ['127.0.0.1', ]
